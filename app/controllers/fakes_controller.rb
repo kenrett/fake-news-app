60f1e45642433588599f6bcb8a7715cd3d08340e
Ken Rettberg <ken.rettberg@devbootcamp.com>
2017-10-14 09:46:45 -0700

    Added fake show and styling

--- app/controllers/fakes_controller.rb
+++ app/controllers/fakes_controller.rb
@@ -9,19 +9,33 @@ class FakesController < ApplicationController
 		@fake = Fake.new
 
 		respond_to do |f|
-			f.js {}
+			f.js
 		end
 	end
 
 	def create
 		@fake = Fake.create(fake_params)
+		# flash[:alerts] = @fake.errors.empty? ? "Error" : @fake.errors.full_messages.to_sentence
 
 		redirect_to :root
+	end
+
+	def show
+		@fake = Fake.find(params[:id])
+	end
+
+	def destroy
+		@fake = current_user.fakes.find(params[:id])
+		@fake.destroy
+
+		respond_to do |f|
+			f.js
+		end
 	end
 
 	private
 
 	def fake_params
-		params.require(:fake).permit(:image)
+		params.require(:fake).permit(:image, :creator_id, :source, :title)
 	end
 end
